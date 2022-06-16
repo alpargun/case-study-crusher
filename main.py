31cf203aa6626f1bcbfc8376d0f0acbeda38b1b7
Alp Argun <alpargun@gmail.com>
2022-06-16 23:12:52 +0200

    add preprocessing

--- main.py
+++ main.py
@@ -8,13 +8,29 @@ import pandas as pd
 
 path = 'Case_study_Crusher.csv'
 
-df = pd.read_csv('Case_study_Crusher.csv', delimiter="\;", encoding = "utf-16", engine='python') # utf-8 does not work. "\;" and engine configuration to fix parser error.
+df = pd.read_csv('Case_study_Crusher.csv', delimiter="\;", encoding = "utf-16", engine='python', quoting=3) # utf-8 does not work. "\;" and engine configuration to fix parser error.
 df
 
-#%%
+#%% Preprocessing
+
+# Remove unnecessary quotes (") from each cell
+
+# Column names
+df.columns = df.columns.str.replace('"', '')
+
+# Cells
+for i, col in enumerate(df.columns):
+    df.iloc[:, i] = df.iloc[:, i].str.replace('"', '')
+df
+
+
+#%% Check if Z121 and H122 times are the same
+df[df.columns[0]].equals(df[df.columns[2]])
 
 
 
 
 
+# %%
+df.columns
 # %%
