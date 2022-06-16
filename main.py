21a4ba4261e3c5f6f4dd7dee0ed8cc0226e45745
Alp Argun <alpargun@gmail.com>
2022-06-16 22:21:55 +0200

    add load data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
-#%%
+#%% Import statements
 
 import pandas as pd
+
+
+#%% Read csv as a dataframe
+
+path = 'Case_study_Crusher.csv'
+
+df = pd.read_csv('Case_study_Crusher.csv', delimiter="\;", encoding = "utf-16", engine='python') # utf-8 does not work. "\;" and engine configuration to fix parser error.
+df
+
+#%%
 
 
 
